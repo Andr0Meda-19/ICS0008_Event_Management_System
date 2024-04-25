f202bd3fd65a17430e2d222e8b98c5ad2497abc3
vakoro <vakoro@taltech.ee>
2024-04-25 21:21:48 +0000

    Replace db_config.php

--- db_config.php
+++ db_config.php
@@ -5,13 +5,13 @@ $dbname = "ICS0008_25";
 $username = "ICS0008_WT_25";
 $password = "8b15d3f168f2";
 
-$mysqli = new mysqli(hostname: $host, 
-                     username: $username, 
-                     password: $password, 
-                     database: $dbname);
+$conn = new mysqli(hostname: $host, 
+                   username: $username, 
+                   password: $password, 
+                   database: $dbname);
 
-if ($mysqli->connect_errno) {
-    die("Failed to connect to MySQL: " . $mysqli->connect_error);
+if ($conn->connect_errno) {
+    die("Failed to connect to MySQL: " . $conn->connect_error);
 }
 
-return $mysqli;
+return $conn;
