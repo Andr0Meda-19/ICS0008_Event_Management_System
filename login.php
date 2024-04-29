b1289442882ed4a44f8c1b9f0359029025d9bc80
vakoro <vakoro@taltech.ee>
2024-04-29 21:05:38 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 session_start(); // Start session to store user login status
 
 require_once('db_config.php'); // Include the database configuration file
