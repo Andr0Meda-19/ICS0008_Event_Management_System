15e02f89ee5307360812e614c8cbbd974c5f656a
vakoro <vakoro@taltech.ee>
2024-04-30 20:32:38 +0000

    Replace add_task.php

--- add_task.php
+++ add_task.php
@@ -14,13 +14,14 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+$task_name = $_POST['task_name'];
 $user_id = $_SESSION['user_id'];
 $task_description = $_POST['task_description'];
 
 // Защита от SQL-инъекций (можно улучшить, в зависимости от используемой библиотеки)
 $task_description = mysqli_real_escape_string($conn, $task_description);
 
-$sql = "INSERT INTO tasks (user_id, task_description) VALUES ('$user_id', '$task_description')";
+$sql = "INSERT INTO tasks (user_id, task_name, task_description) VALUES ('$user_id', '$task_name', '$task_description')";
 
 if (mysqli_query($conn, $sql)) {
     echo "Task added successfully";
