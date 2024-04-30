86e9d70cd4045e8f3fa0ef6fd66a426dc69aa04a
vakoro <vakoro@taltech.ee>
2024-04-30 21:50:28 +0000

    Update file index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
                 <?php
                 session_start();
                 if(isset($_SESSION['user_id'])) {
-                    echo '<a href="logout.php">Log out</a>';
+                    echo '<a href="php/logout.php">Log out</a>';
                 } else {
                     echo '<a href="login.php">Log in</a>';
                 }
