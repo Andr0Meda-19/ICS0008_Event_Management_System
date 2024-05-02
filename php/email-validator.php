a372a4922de6a7387c82da54f0f0fedf65010a53
vakoro <vakoro@taltech.ee>
2024-05-02 15:37:29 +0000

    Upload New File

---
+++ php/email-validator.php
@@ -0,0 +1,17 @@
+<?php
+
+$conn = require __DIR__ . "/db_config.php";
+
+$sql = sprintf("SELECT * FROM user
+                WHERE email = '%s'",
+                $conn->real_escape_string($_GET["email"]));
+                
+$result = $conn->query($sql);
+
+$is_available = $result->num_rows === 0;
+
+header("Content-Type: application/json");
+
+echo json_encode(["available" => $is_available]);
+
+?>
