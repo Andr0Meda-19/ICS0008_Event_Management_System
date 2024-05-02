6fff3560cb1fab86816bc59f752d819d30ea7736
antimo <antimo@taltech.ee>
2024-05-02 03:54:20 +0300

    added style and logic for navbar and navigation arrows

--- index.php
+++ index.php
@@ -4,41 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <title>Add task</title>
 </head>
 <body>
     <header>
-        <div class="navigation">
-            <div class="navigation-content">
-                <img src="./svgs/logo_svg-01.svg" alt="logo">
-            </div>
-        </div>
-        <div class="navigation-links">
-            <nav>
-                <a href="index.php">Add Task</a>
-                <a href="tasklist.php">Task List</a>
-                <a href="about.html">About</a>
+        <nav>
+            <img src="./svgs/logo_svg-01.svg" class="logo" alt="logo">
+        
+            <ul class="navbar">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="index.php">Add Task</a></li>
+                <li><a href="tasklist.php">Task List</a></li>
+                <li><a href="about.html">About Us</a></li>
+            </ul>
+            <div class="main">
                 <?php
-                session_start();
-                if(isset($_SESSION['user_id'])) {
-                    echo '<a href="php/logout.php">Log out</a>';
-                } else {
-                    echo '<a href="login.php">Log in</a>';
-                }
+                    session_start();
+                    if(isset($_SESSION['user_id'])) {
+                        echo '<a href="php/logout.php" class="user"><i class="ri-user-fill"></i>Log Out</a>';
+                    } else {
+                        echo '<a href="login.php" class="user"><i class="ri-user-fill"></i>Sign In</a>';
+                    }
                 ?>
-            </nav>
-        </div>
+                <!-- <a href="login.php" class="user"><i class="ri-user-fill"></i>Sign in</a> -->
+                <div class="bx bx-menu" id="menu-icon"></div>
+            </div>
+        </nav>
     </header>
-    <div class="add-task-container">
     <h2>Add Task</h2>
-    <form action="./php/add_task.php" method="POST">
-        <label>Task name:</label><br>
-        <input type="text" name="task_name" required><br>
-        <label>Description:</label><br>
-        <textarea name="task_description"></textarea><br>
-        <button type="submit">Add Task</button>
-    </form>
+    <div class="add-task-container">
+        <form action="./php/add_task.php" id="add_task_form" method="POST">
+            <label for="task_name">Task name:</label>
+            <input type="text" id="task_name" name="task_name" required>
+            <label for="task_description">Description:</label>
+            <textarea id="task_description" name="task_description"></textarea>
+            <button type="submit" id="submit_task">Add Task</button>
+        </form>
     </div>
+    <script src="scripts/navbar.js"></script>
     <script src="scripts/navigation.js"></script>
 </body>
 </html>
--- tasklist.php
+++ tasklist.php
@@ -4,31 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <title>Task List</title>
 </head>
 <body>
     <header>
-        <div class="navigation">
-            <div class="navigation-content">
-                <img src="./svgs/logo_svg-01.svg" alt="logo">
-            </div>
-        </div>
-        <div class="navigation-links">
-            <nav>
-                <a href="index.php">Add Task</a>
-                <a href="tasklist.php">Task List</a>
-                <a href="about.html">About</a>
+        <nav>
+            <img src="./svgs/logo_svg-01.svg" class="logo" alt="logo">
+
+            <ul class="navbar">
+            <li><a href="index.php">Home</a></li>
+                <li><a href="index.php">Add Task</a></li>
+                <li><a href="tasklist.php">Task List</a></li>
+                <li><a href="about.html">About Us</a></li>
+            </ul>
+
+            <div class="main">
                 <?php
-                session_start();
-                // Redirect user to login page if they are not logged in
-                if (!isset($_SESSION['user_id'])) {
-                    header("Location: login.php");
-                    exit(); // Ensure no further execution if not logged in
-                }
-                echo '<a href="php/logout.php">Log out</a>';
+                    session_start();
+                    // Redirect user to login page if they are not logged in
+                    if (!isset($_SESSION['user_id'])) {
+                        header("Location: login.php");
+                        exit(); // Ensure no further execution if not logged in
+                    }
+                    echo '<a href="php/logout.php" class="user"><i class="ri-user-fill"></i>Log Out</a>';
                 ?>
-            </nav>
-        </div>
+                <!-- <a href="login.php" class="user"><i class="ri-user-fill"></i>Sign in</a> -->
+                <div class="bx bx-menu" id="menu-icon"></div>
+            </div>
+
+        </nav>
     </header>
     <table>
         <tr>
@@ -60,6 +66,7 @@
         }
         ?>
     </table>
+    <script src="scripts/navbar.js"></script>
     <script src="scripts/navigation.js"></script>
 </body>
 </html>
